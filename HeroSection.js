5dbac21f7e26fea1e9d60b284057b0b68e579c5b
sakshee44566 <118666046+sakshee44566@users.noreply.github.com>
2025-04-11 01:32:06 +0530

    Delete HeroSection.js

--- HeroSection.js
+++
@@ -1,9 +0,0 @@
-const HeroSection = () => (
-    <div className="text-center p-10 bg-blue-500 text-white">
-      <h1 className="text-4xl font-bold mb-2">Welcome to MyStore</h1>
-      <p className="text-lg">Shop the latest products at unbeatable prices.</p>
-    </div>
-  );
-  
-  export default HeroSection;
-  
